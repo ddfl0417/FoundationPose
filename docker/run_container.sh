23a396bd6e5032d6ff784666cbaa8c71842fe106
ddfl0417 <48817585+ddfl0417@users.noreply.github.com>
2024-08-20 21:34:51 +0800

    Modified run_container.sh

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,3 +1,17 @@
+#!/bin/bash
+
+# Remove any existing container named foundationpose
 docker rm -f foundationpose
-DIR=$(pwd)/../
-xhost +  && docker run --gpus all --env NVIDIA_DISABLE_REQUIRE=1 -it --network=host --name foundationpose  --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v $DIR:$DIR -v /home:/home -v /mnt:/mnt -v /tmp/.X11-unix:/tmp/.X11-unix -v /tmp:/tmp  --ipc=host -e DISPLAY=${DISPLAY} -e GIT_INDEX_FILE foundationpose:latest bash -c "cd $DIR && bash"
+
+# Set the current directory
+DIR=$(pwd)/
+
+# Allow connections to the X server
+xhost +
+
+# Run the Docker container using the modified image
+docker run --gpus all --env NVIDIA_DISABLE_REQUIRE=1 -it --network=host --name foundationpose \
+  --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
+  -v $DIR:$DIR -v /home:/home -v /mnt:/mnt -v /tmp/.X11-unix:/tmp/.X11-unix -v /tmp:/tmp \
+  --ipc=host -e DISPLAY=${DISPLAY} -e GIT_INDEX_FILE \
+  ddfl0417/foundationpose:ubuntu-22.04 bash -c "cd $DIR && bash"
