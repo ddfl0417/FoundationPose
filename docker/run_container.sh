a65af32d57c992f771aa1bba5edf15b3da4f77c7
ddfl0417 <48817585+ddfl0417@users.noreply.github.com>
2024-08-21 11:30:51 +0800

    Update run_container.sh

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -14,4 +14,5 @@ docker run --gpus all --env NVIDIA_DISABLE_REQUIRE=1 -it --network=host --name f
   --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
   -v $DIR:$DIR -v /home:/home -v /mnt:/mnt -v /tmp/.X11-unix:/tmp/.X11-unix -v /tmp:/tmp \
   --ipc=host -e DISPLAY=${DISPLAY} -e GIT_INDEX_FILE \
+  --device=/dev/dri:/dev/dri \
   ddfl0417/foundationpose:ubuntu-22.04 bash -c "cd $DIR && bash"
