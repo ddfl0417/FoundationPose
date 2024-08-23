94fefd73a35391e4fcd1da0c5a4f6506da8acfe0
ddfl0417 <48817585+ddfl0417@users.noreply.github.com>
2024-08-23 21:28:03 +0800

    Updated docker files

---
+++ docker/build_image.sh
@@ -0,0 +1,2 @@
+BASEDIR=$(cd $(dirname $0) && pwd)
+docker build -t ddfl0417/foundationpose:ubuntu-22.04 $BASEDIR
--- docker/run_container.sh
+++ docker/run_container.sh
@@ -12,7 +12,9 @@ xhost +
 # Run the Docker container using the modified image
 docker run --gpus all --env NVIDIA_DISABLE_REQUIRE=1 -it --network=host --name foundationpose \
   --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
-  -v $DIR:$DIR -v /home:/home -v /mnt:/mnt -v /tmp/.X11-unix:/tmp/.X11-unix -v /tmp:/tmp \
+  -v $DIR:$DIR -v /home:/home -v /mnt:/mnt -v /tmp/.X11-unix:/tmp/.X11-unix -v /tmp:/tmp -v /dev:/dev \
   --ipc=host -e DISPLAY=${DISPLAY} -e GIT_INDEX_FILE \
   --device=/dev/dri:/dev/dri \
+  --device-cgroup-rule "c 81:* rmw" \
+  --device-cgroup-rule "c 189:* rmw" \
   ddfl0417/foundationpose:ubuntu-22.04 bash -c "cd $DIR && bash"
